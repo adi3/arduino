307e86e6ae69ef559dc0034bd0198d061f253246
Aditya Singh <adisin8@gmail.com>
2012-10-23 23:29:02 +0530

    Updated readme.

--- Obstacle_Avoiding_Robot/Obstacle_Avoiding_Robot.ino
+++ Obstacle_Avoiding_Robot/Obstacle_Avoiding_Robot.ino
@@ -5,11 +5,13 @@
 
 // pin numbers
 const int inputPin = A0;
-const int pin1A =  13;
-const int pin1B =  12;
+const int pin1A =  12;
+const int pin1B =  13;
 const int pin2A =  11;
 const int pin2B =  10;
-const int threshold = 200;
+const int threshold = 600;
+
+int turn = 0;
 
 void setup() {
     // initializes serial communication at 9600 bits per second
@@ -28,17 +30,15 @@ void loop(){
   int sensorValue = analogRead(A0);
   
   // checks input and sets LED state accordingly
-  if (sensorValue < threshold)
-    driveForward();
-  else
-    
-    
- 
-
-    // reverses signal for both LEDs by setting to state and !state.
- //   digitalWrite(pin1, state);
- //   digitalWrite(pin2, !state);
-    
+  if (sensorValue < threshold) {
+      driveForward();
+  } else {
+      turn = random(2);
+      if (turn == 0)
+        turnLeft();
+      if (turn == 1)
+        turnRight();
+  }
   // print out input value after normalization
   Serial.println(sensorValue);
   delay(100);        // delay in between reads for stability
@@ -46,10 +46,27 @@ void loop(){
 }
 
 void driveForward(){
+    digitalWrite(pin1A, LOW);
+    digitalWrite(pin1B, HIGH);
+    digitalWrite(pin2A, LOW);
+    digitalWrite(pin2B, HIGH);
+}
+
+
+void turnRight(){
     digitalWrite(pin1A, HIGH);
     digitalWrite(pin1B, LOW);
+    digitalWrite(pin2A, LOW);
+    digitalWrite(pin2B, HIGH);
+    delay(1800);
+}
+
+void turnLeft(){
+    digitalWrite(pin1A, LOW);
+    digitalWrite(pin1B, HIGH);
     digitalWrite(pin2A, HIGH);
     digitalWrite(pin2B, LOW);
+    delay(1800);
 }
 
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# [ARDUINO] (http://arduino.cc/)
+A place for all my Arduino sketches. I mostly use these to build robots and other simple utility gadgets.
