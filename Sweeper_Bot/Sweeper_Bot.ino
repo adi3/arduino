b5445417ac61a22b8fd86d4f851aeb39691684d8
Aditya Singh <adisin8@gmail.com>
2012-12-05 00:02:50 +0530

    Added locomotion functions.

--- Sweeper_Bot/Sweeper_Bot.ino
+++ Sweeper_Bot/Sweeper_Bot.ino
@@ -13,47 +13,77 @@ int sensorPin = 1;  // analog input pin for infrared sensor
 int val;            // variable to read the value from the analog pin
 int pos = 0;        // variable to store the servo position 
 
+// pins for motor signals
+int oneA = 4;
+int oneB = 2;
+int twoA = 8;
+int twoB = 7;
+
 void setup() 
 { 
   myservo.attach(9);      // attaches the servo on pin 9 to the servo object
-  myservo.write(90);      // makes servo/sensor face forward.
+  myservo.write(80);      // makes servo/sensor face forward.
   Serial.begin(9600);
+  
+  // initialize the digital motor pins as output.
+  pinMode(oneA, OUTPUT);  
+  pinMode(oneB, OUTPUT);  
+  pinMode(twoA, OUTPUT);  
+  pinMode(twoB, OUTPUT);    
 } 
 
 void loop() 
 {
-    myservo.write(90);
+    myservo.write(80);
+    if(isClear(400)){
+      Serial.println("forward"); 
+      moveForward();
+    } else {
+      Serial.println("back!"); 
+      moveBack();
+    }
+    delay(500);
+/*    myservo.write(80);
     delay(1000);
 
-  val = analogRead(sensorPin);
-  String result = "Front: " + String(val);
-  Serial.println(result); 
-  if(val < 500) {
-    Serial.println("forward clear"); 
-  } 
-  else {
-    Serial.println("forward blocked");   
-  }
+    val = analogRead(sensorPin);
+ //   String result = "Front: " + String(val);
+    if(val < 400) {
+      Serial.println("forward clear\n"); 
+    } 
+    else {
+      Serial.println("forward blocked\n");   
+    }
 
     myservo.write(0);
     delay(1000);                           // waits for the servo to get there 
+  
+    val = analogRead(sensorPin);
+//    result = "Left: " + String(val);
+    if(val < 600) { 
+      Serial.write("left clear\n"); 
+    } else {
+      Serial.println("left blocked\n");   
+    }*/
+}
 
-  val = analogRead(sensorPin);
-  result = "Left: " + String(val);
-  Serial.println(result); 
-  //   val = analogRead(sensorPin);
-  //  val = map(val, 0, 1023, 0, 179);
+void moveForward(){
+  digitalWrite(oneA, HIGH);
+  digitalWrite(oneB, LOW);
+  digitalWrite(twoA, HIGH);
+  digitalWrite(twoB, LOW);
+}
 
-  //  if(val < 100) {
-  //    Serial.println(val); 
-  //    Serial.write("left clear"); 
-  //  } else {
-  ///Serial.println("left blocked");   
-  //  }
+void moveBack(){
+  digitalWrite(oneB, HIGH);
+  digitalWrite(oneA, LOW);
+  digitalWrite(twoB, HIGH);
+  digitalWrite(twoA, LOW);
+}
 
-  //    myservo.write(90);                 
-  // } else {
-  //   myservo.write(0);
-  // }
-} 
+boolean isClear(int threshold){
+    val = analogRead(sensorPin);
+    return (val < threshold);
+}
+
 
